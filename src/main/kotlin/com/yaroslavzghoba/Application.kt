e8ccb24a04bb004e1785bf021da00cfa854e67a8
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2024-12-14 18:46:38 +0100

    Added an entry point to the server application

---
+++ src/main/kotlin/com/yaroslavzghoba/Application.kt
@@ -0,0 +1,12 @@
+package com.yaroslavzghoba
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+@Suppress("unused", "UnusedReceiverParameter")
+fun Application.module() {
+
+}
